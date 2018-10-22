fd2e3221e216f37874e426c9f7da99a7f22719d6
tbreuss <thomasbreuss@gmx.ch>
2018-10-22 20:49:08 +0200

    Trim leading slash from namespace

--- example/public/index.php
+++ example/public/index.php
@@ -17,11 +17,11 @@ Tebe\Adroit\Application::instance()
         Equip\Handler\ActionHandler::class,
     ])*/
     ->setRoutes([
-        ['GET', '/', \Example\Ui\Web\IndexAction::class],
-        ['GET', '/blog', \Example\Ui\Web\Blog\BlogIndexAction::class],
-        ['GET', '/blog/add', \Example\Ui\Web\Blog\BlogAddAction::class],
-        ['GET', '/blog/{id:[a-z0-9-]+}', \Example\Ui\Web\Blog\BlogDetailAction::class],
-        ['POST', '/blog/create', \Example\Ui\Web\Blog\BlogCreateAction::class],
-        ['GET', '/blog/{id:[a-z0-9-]+}/delete', \Example\Ui\Web\Blog\BlogDeleteAction::class]
+        ['GET', '/', Example\Ui\Web\IndexAction::class],
+        ['GET', '/blog', Example\Ui\Web\Blog\BlogIndexAction::class],
+        ['GET', '/blog/add', Example\Ui\Web\Blog\BlogAddAction::class],
+        ['GET', '/blog/{id:[a-z0-9-]+}', Example\Ui\Web\Blog\BlogDetailAction::class],
+        ['POST', '/blog/create', Example\Ui\Web\Blog\BlogCreateAction::class],
+        ['GET', '/blog/{id:[a-z0-9-]+}/delete', Example\Ui\Web\Blog\BlogDeleteAction::class]
     ])
     ->run();
